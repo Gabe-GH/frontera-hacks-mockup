7c1cc2d801eb4c4f1a38bc7383f7fc94f24c0431
Gabe-GH <gabriel.lira01@utrgv.edu>
2024-08-13 05:37:07 -0500

    scripts to process and extract data required for registration form options

--- app/scripts/writeDataToJson.ts
+++ app/scripts/writeDataToJson.ts
@@ -1,44 +1,14 @@
-import { parse } from "csv-parse";
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
+import { processCountriesToArray } from "./processCountries";
+import { processCsvToArray } from "./processCsvToArray";
+
 const __filename: string = fileURLToPath(import.meta.url);
 const __dirname: string = path.dirname(__filename);
 
-const csvFilePath: string = path.join(__dirname, "../data/schools.csv");
-
-/**
- * Asynchronously processes a CSV file and extracts data.
- *
- * This function reads a CSV file from the provided file path, parses it, and extracts
- * the first column of each row into an array of strings. It handles the file reading
- * and parsing asynchronously, making it efficient for large datasets.
- *
- * @async
- * @function processCSV
- * @param {string} filePathToCSV - The file path to the CSV file to be processed.
- * @returns {Promise<string[]>} - A promise that resolves to an array of strings, where each string represents a data entry from the CSV file.
- *
- * @throws {Error} Will throw an error if the file cannot be read or parsed.
- *
- * @example
- * // Example usage
- * processCSV(csvFilePath).then(data => console.log(data));
- *
- * // Expected output (if CSV contains school names):
- * // ['School1', 'School2', 'School3', ...]
- */
-const processCSV = async (filePathToCSV: string): Promise<string[]> => {
-  const schools: string[] = [];
-  const parser = fs.createReadStream(filePathToCSV).pipe(parse());
-
-  for await (const entry of parser) {
-    schools.push(entry[0]);
-  }
-
-  return schools;
-};
+const schoolCsvFilePath: string = path.join(__dirname, "../data/schools.csv");
 
 /**
  * Converts an array of data into JSON format and writes it to a file.
@@ -48,7 +18,7 @@ const processCSV = async (filePathToCSV: string): Promise<string[]> => {
  * ../data directory, and the function ensures that the directory exists.
  *
  * @async
- * @function convertDataToJSON
+ * @function writeDataToJSON
  * @param {string[]} data - An array of strings representing data entries.
  * @param {string} jsonFileTargetName - The desired name for the output JSON file (without extension).
  * @returns {Promise<void>} - A promise that resolves when the JSON data has been successfully written to the file.
@@ -57,14 +27,14 @@ const processCSV = async (filePathToCSV: string): Promise<string[]> => {
  *
  * @example
  * // Example usage
- * convertDataToJSON(['School1', 'School2'], 'schools')
+ * writeDataToJSON(['School1', 'School2'], 'schools')
  *   .then(() => console.log('Data successfully converted to JSON'))
  *   .catch(err => console.error('Error:', err));
  *
  * // Expected output:
  * // JSON data successfully written to ../data/schools.json
  */
-const convertDataToJSON = async (
+const writeDataToJSON = async (
   data: string[],
   jsonFileTargetName: string
 ): Promise<void> => {
@@ -88,6 +58,7 @@ console.log("Starting script...");
 
 // NOTE: This is the main function that runs the script and writes the JSON file
 (async () => {
-  const data = await processCSV(csvFilePath);
-  const product = await convertDataToJSON(data, "schools");
+  await writeDataToJSON(await processCsvToArray(schoolCsvFilePath), "schools");
+
+  await writeDataToJSON(processCountriesToArray(), "countries");
 })();
---
+++ app/util/getSchoolData.ts
@@ -0,0 +1,77 @@
+"use server";
+
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+interface DataObj {
+  data: string[];
+  msgErr: string | null;
+}
+
+const errorStrings: string[] = [
+  "No data found for local school json file",
+  "Local school jsonfile not found",
+  "Error parsing JSON data from local school json file",
+  "Invalid data format for local school json file",
+  "An unexpected error occured while loading school data",
+];
+
+const __filename: string = fileURLToPath(import.meta.url);
+const __dirname: string = path.dirname(__filename);
+
+/**
+ * Retrieves data from a local JSON file and handles potential errors.
+ *
+ * This function reads a JSON file located in the ../data directory, attempts to parse its content,
+ * and returns the data along with any errors encountered during the process. It covers scenarios such as:
+ * - Missing JSON file.
+ * - Syntax errors in the JSON content.
+ * - Unexpected data formats (e.g., non-array data).
+ *
+ * @async
+ * @function getListFromJsonFile
+ * @param {string} fileName - The name of the JSON file (without the extension) to be read.
+ * @returns {Promise<DataObj>} - A promise that resolves to an object containing the parsed data and any error message.
+ *
+ * @example
+ * // Fetch school names from the JSON file
+ * const result = await getListFromJsonFile('schoolNames');
+ * console.log(result);
+ * // Expected output:
+ * // { data: ['School1', 'School2'], msgErr: null }
+ *
+ * // Fetch country names from the JSON file
+ * const result = await getListFromJsonFile('countryNames');
+ * console.log(result);
+ * // Expected output:
+ * // { data: ['Country1', 'Country2'], msgErr: null }
+ */
+export const getListFromJsonFile = async (
+  fileName: string
+): Promise<DataObj> => {
+  let data: string[] = [];
+  let msgErr: string | null = null;
+
+  try {
+    const jsonDataFile = path.join(__dirname, `../data/${fileName}.json`);
+    const fileContent = await fs.readFile(jsonDataFile, "utf-8");
+    const schoolData = JSON.parse(fileContent);
+
+    if (!Array.isArray(schoolData))
+      throw new Error(`Invalid data format for local school json file`);
+
+    data = schoolData;
+
+    if (schoolData.length === 0) msgErr = errorStrings[0];
+  } catch (error: any) {
+    if (error.code === "ENOENT") msgErr = errorStrings[1];
+    if (error.name === "SyntaxError") msgErr = errorStrings[2];
+    if (error.message.includes("Invalid data fromat")) msgErr = errorStrings[3];
+    else msgErr = errorStrings[4];
+
+    console.error("Error loading school data:", error.message);
+  }
+
+  return { data, msgErr };
+};
